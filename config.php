eb1564a5115436ae3b34a354de562e2ea15f3cdf
Theubald-bee <thibaut.matian@gmail.com>
2025-09-29 13:16:21 +0200

    config de bdd

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "moduleconnexion";
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
